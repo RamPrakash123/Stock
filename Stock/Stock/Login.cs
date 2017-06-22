3bf023856802df2f65c178d492115586bf4819e9
RamPrakash123 <a.prakash@scrapq.com>
2017-06-22 14:46:35 +0530

    Initial Commit

---
+++ Stock/Stock/Login.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //to do Check whether login & password are correct or not
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT *
+                                 FROM[dbo].[Login] where UserName = '"+ textBox1.Text + "' and Password = '"+ textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login ID and Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1_Click(sender, e);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+    }
+}
